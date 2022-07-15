4ae1c0284a8944a29d9afedaee2201f46d549e44
koyabu <xdaisuke0724@gmail.com>
2022-07-15 16:08:53 +0900

    足し算機能追加

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,18 @@ const app = Vue.createApp({
     data(){
         return{
             title:"Vue",
-
+            input1:"",
+            input2:"",
+            answer:0,
+            flg:false,
+        }
+    },
+    methods:{
+        add(){
+            if(!isNaN(this.input1) && !isNaN(this.input2)){
+                this.flg = true;
+                this.answer = Number(this.input1) + Number(this.input2); 
+            }   
         }
     }
 }).mount("#app");
